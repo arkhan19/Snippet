9d84f8bdf80aa46809c78ccca956a9b56ccd59df
f3n1xx <arkspeed@gmail.com>
2018-11-14 13:29:14 +0530

    2. Requests and Responses

--- Snippet/urls.py
+++ Snippet/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path
 from snippets import views
 
+from rest_framework.urlpatterns import format_suffix_patterns
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 
@@ -24,3 +26,5 @@ urlpatterns = [
     path('snippets/', views.snippet_list),
     path('snippets/<int:pk>', views.snippet_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
